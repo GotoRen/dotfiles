3443beff9ce9598dfceb3ef2157c2541056d5dc9
GotoRen <r0719en@pluslab.org>
2022-08-11 10:45:44 +0900

    🎉 Initial commit.

--- bin/ln.sh
+++ bin/ln.sh
@@ -1,12 +1,24 @@
 #!/bin/bash -e
 
-DOTPATH=~/dotfiles
+DOTPATH=$HOME/dotfiles
 
 ### Todo: check existence.
-ln -s $DOTPATH/.zshrc ~/.zshrc
-ln -s $DOTPATH/.ssh_config ~/.ssh/config
-ln -s $DOTPATH/.gitconfig ~/.gitconfig
-ln -s $DOTPATH/.gitignore_global ~/.gitignore_global
-ln -s $DOTPATH/starship.toml ~/.config/starship.toml
-ln -s $DOTPATH/bin/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
-ln -s $DOTPATH/bin/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+ln -s $DOTPATH/.zshrc $HOME/.zshrc
+ln -s $DOTPATH/.gitconfig $HOME/.gitconfig
+ln -s $DOTPATH/.gitignore_global $HOME/.gitignore_global
+
+ln -s $DOTPATH/bin/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
+ln -s $DOTPATH/bin/vscode/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
+
+ln -s $DOTPATH/.ssh_config $HOME/.ssh/config
+
+ln -s $DOTPATH/starship.toml $HOME/.config/starship.toml
+
+
+# ls -a | grep -Ev "README|bin|\.$|^\.git$" | while read f; do
+#   if [ $f = ".ssh_config"  ]; then
+#     ln -s $f $HOME/.ssh/config
+#   else
+#     ln -s $f $HOME/
+#   fi
+# done
