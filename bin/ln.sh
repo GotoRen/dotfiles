d9929c981f7635a12a3dddd2701d9584b66eab22
GotoRen <r0719en@pluslab.org>
2022-09-01 03:24:22 +0900

    ln fix.

--- bin/ln.sh
+++ bin/ln.sh
@@ -7,23 +7,23 @@ VSCODEPATH="${BINPATH}/vscode"
 
 ### /bin/vscode
 ls -a $VSCODEPATH | grep -E '.json' | while read f; do
-  ln -s $VSCODEPATH/$f $HOME/Library/Application\ Support/Code/User/$f
+  ln -snfv $VSCODEPATH/$f $HOME/Library/Application\ Support/Code/User/$f
 done
 
 ### /vim
 ls -a $VIMPATH | grep -Ev "\.$" | while read f; do
-  ln -s $VIMPATH/$f $HOME/$f
+  ln -snfv $VIMPATH/$f $HOME/$f
 done
 
 ### special files.
 ls -a $DOTPATH | grep -Ev "README|bin|vim|\zsh$|vscode|\.$|^\.git$|Brewfile|LICENSE|Makefile" | while read f; do
   if [ $f = ".ssh_config"  ]; then
-    sudo ln -s $DOTPATH/$f $HOME/.ssh/config
+    sudo ln -snfv $DOTPATH/$f $HOME/.ssh/config
   elif [ $f = "starship.toml"  ]; then
-    sudo ln -s $DOTPATH/$f $HOME/.config/$f
+    sudo ln -snfv $DOTPATH/$f $HOME/.config/$f
   else
-    echo $f
-    sudo ln -s $DOTPATH/$f $HOME/$f
+    sudo ln -snfv $DOTPATH/$f $HOME/$f
   fi
   git config --global core.excludesfile $HOME/.gitignore_global
 done
+
